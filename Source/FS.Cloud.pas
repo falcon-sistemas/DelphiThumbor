0d1f4d8fe01eacce069877f5612eabf55b4635dd
Marlon Nardi <marlonnardi@hotmail.com>
2022-04-22 08:22:25 -0400

    * Added new methods

--- Source/FS.Cloud.pas
+++ Source/FS.Cloud.pas
@@ -10,7 +10,9 @@ unit FS.Cloud;
 interface
 
 {$IF Defined(ANDROID) OR Defined(IOS)}
-  {$DEFINE INDY}
+  {$IF CompilerVersion < 34.0}
+    {$DEFINE INDY}
+  {$ENDIF}
 {$ENDIF}
 {$IF CompilerVersion < 29.0}
   {$DEFINE INDY}
@@ -36,7 +38,8 @@ uses
   System.Classes,
   System.SysUtils,
   System.IOUtils,
-  System.Generics.Collections;
+  System.Generics.Collections,
+  System.json;
 
 const
   FUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0';
@@ -75,7 +78,7 @@ type
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
 
-    function Get(const AURL: string): string; overload;
+    function Get(const AURL: string; AToken:string = ''): string; overload;
     function Get(const AURL: string; Encondig: TEncoding): string; overload;
     function Get(const AURL: string; const AResponseContent: TStream): string; overload;
     function Get(const AURL: string; const AResponseContent: TStream; AHeader: TObjectList<TFSHeader>): string; overload;
@@ -83,12 +86,18 @@ type
     function Post(AURL: string; ASource: TStrings): string; overload;
     function Post(AURL: string; ASource: TStrings; Encondig: TEncoding): string; overload;
     function Post(AURL: string; ASource: string; AHeader: TObjectList<TFSHeader>): string; overload;
+    function Post(AURL: string; AJson:TJSONObject): string; overload;
+
+    function Put(AURL: string; AJson:TJSONObject): string;
+
+    function Delete(AURL: string): string;
 
     procedure SetContentType(Content: string);
     procedure SetTimeout(Timeout: Integer);
     procedure SetAccept(Accept: string);
     procedure SetAcceptLanguage(AcceptLanguage: string);
     procedure SetUserAgent(UserAgent: string);
+    function Path(AURL, ASource: string; AHeader: TObjectList<TFSHeader>): string;
 
     property Params: TStringList read FParams write FParams;
     property Headers: TObjectList<TFSHeader> read FHeaders write FHeaders;
@@ -105,6 +114,47 @@ begin
 end;
 {$ENDIF}
 
+function TFSCloud.Path(AURL: string; ASource: string; AHeader: TObjectList<TFSHeader>): string;
+{$IFNDEF INDY}
+var
+  vSource: TStream;
+  vResponseContent: TStream;
+  ArrayNetHeader: TNameValueArray;
+  NetHeader: TNetHeader;
+  vI: Integer;
+{$ENDIF}
+begin
+  try
+    {$IFNDEF INDY}
+    vSource := TStringStream.Create(ASource, TEncoding.UTF8);
+    vResponseContent := TStringStream.Create('', TEncoding.UTF8);
+    try
+      SetLength(ArrayNetHeader, AHeader.Count);
+
+      for vI := 0 to AHeader.Count -1 do
+      begin
+        NetHeader := TNetHeader.Create(Headers[vI].Key, Headers[vI].Value);
+        ArrayNetHeader[vI]:= NetHeader;
+      end;
+
+
+      Result := FNetHttp.Patch(AURL, vSource, vResponseContent, ArrayNetHeader).ContentAsString(TEncoding.ANSI);
+    finally
+      FreeAndNil(vSource);
+      FreeAndNil(vResponseContent);
+    end;
+
+    {$ELSE}
+    FIdHttp.Request.Referer := AURL;
+    Result := FIdHttp.Post(AURL, ASource);
+    {$ENDIF}
+  except
+    on e: Exception do
+      raise EFSCloudException.Create(e.Message);
+  end;
+
+end;
+
 constructor TFSCloud.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
@@ -117,23 +167,40 @@ begin
   FNetHttp.ContentType := FContentType;
   FNetHttp.Accept := FAccept;
   FNetHttp.OnValidateServerCertificate := CertValid;
+  {$IF CompilerVersion > 31.0}
   FNetHttp.SecureProtocols := [THTTPSecureProtocol.TLS12];
+  {$ENDIF}
   {$ELSE}
     {$IF Defined(ANDROID)}
     IdOpenSSLSetLibPath(TPath.GetDocumentsPath);
     {$ENDIF}
   FIdHttp := TIdHTTP.Create(Self);
   FIdSsl := TIdSSLIOHandlerSocketOpenSSL.Create(Self);
-  FIdSsl.SSLOptions.Method := sslvTLSv1_2;
-
   FIdHttp.IOHandler := FIdSsl;
-
+  FIdSsl.SSLOptions.Method := sslvTLSv1_2;
   FIdHttp.Request.UserAgent := FUserAgent;
   FIdHttp.Request.ContentType := FContentType;
   FIdHttp.Request.Accept := FAccept;
   {$ENDIF}
 
   Self.SetTimeout(60000);
+end;
+
+function TFSCloud.Delete(AURL: string): string;
+begin
+  try
+
+    {$IFNDEF INDY}
+    Result := FNetHttp.Delete(AURL).ContentAsString(TEncoding.UTF8);
+    {$ELSE}
+    FIdHttp.Request.Referer := AURL;
+    Result := FIdHttp.Delete(AURL);
+    {$ENDIF}
+  except
+    on e: Exception do
+      raise EFSCloudException.Create(e.Message);
+  end;
+
 end;
 
 destructor TFSCloud.Destroy;
@@ -163,11 +230,25 @@ begin
   end;
 end;
 
-function TFSCloud.Get(const AURL: string): string;
+function TFSCloud.Get(const AURL: string; AToken:string = ''): string;
+{$IFNDEF INDY}
+var
+ Headers: TNetHeaders;
+{$ENDIF}
 begin
   try
     {$IFNDEF INDY}
-    Result := FNetHttp.Get(AURL).ContentAsString(TEncoding.ANSI);
+    if AToken.IsEmpty then
+      Result := FNetHttp.Get(AURL).ContentAsString(TEncoding.ANSI)
+    else
+    begin
+
+      SetLength(Headers,1);
+      Headers[0].Name:= 'Authorization';
+      Headers[0].Value:= 'Bearer '+AToken;
+
+      Result := FNetHttp.Get(AURL,nil,Headers).ContentAsString(TEncoding.ANSI);
+    end;
     {$ELSE}
     Result := FIdHttp.Get(AURL);
     {$ENDIF}
@@ -226,6 +307,46 @@ begin
     on e: Exception do
       raise EFSCloudException.Create(e.Message);
   end;
+end;
+
+function TFSCloud.Post(AURL: string; AJson: TJSONObject): string;
+var
+  vSource: TStringStream;
+begin
+  try
+
+    vSource:=  TStringStream.Create(AJson.ToString);
+    {$IFNDEF INDY}
+    Result := FNetHttp.Post(AURL,vSource).ContentAsString(TEncoding.UTF8);
+    {$ELSE}
+    FIdHttp.Request.Referer := AURL;
+    Result := FIdHttp.Post(AURL, vSource);
+    {$ENDIF}
+  except
+    on e: Exception do
+      raise EFSCloudException.Create(e.Message);
+  end;
+
+end;
+
+function TFSCloud.Put(AURL: string; AJson: TJSONObject): string;
+var
+  vSource: TStringStream;
+begin
+  try
+
+    vSource:=  TStringStream.Create(AJson.ToString);
+    {$IFNDEF INDY}
+    Result := FNetHttp.Put(AURL,vSource).ContentAsString(TEncoding.UTF8);
+    {$ELSE}
+    FIdHttp.Request.Referer := AURL;
+    Result := FIdHttp.Put(AURL, vSource);
+    {$ENDIF}
+  except
+    on e: Exception do
+      raise EFSCloudException.Create(e.Message);
+  end;
+
 end;
 
 function TFSCloud.Post(AURL: string; ASource: TStrings;
